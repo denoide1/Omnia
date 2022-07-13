9b62f5c9c1eafb8fa78941bb182d1936fcad0bf1
Gianmarco Rampulla <45428938+denoide1@users.noreply.github.com>
2022-07-13 14:39:07 +0200

    Started Parser

--- omnia_parser.h
+++ omnia_parser.h
@@ -15,15 +15,92 @@ public:
             this->tokens = tokens;
         }
 
-        void parse() {
+        int parse() {
 
+            int value = 0;
+            while (i < this->tokens.size()) {
+                fetch();
+                switch (next_token.first) {
+                    case "WRITE":
+                        value = write();
+                        break;
+                    case "WORD":
+                        value = -1;
+                        break;
+                    case "ENDPHRASE":
+                        value =  -1;
+                        break;
+                    case "NUMBER":
+                        value = -1;
+                        break;
 
+                }
+            }
+
+            return value;
 
         }
 
 private:
     std::vector<std::pair<std::string,std::string>> tokens;
+    std::pair<std::string,std::string> next_token;
+    int i = 0;
 
+    fetch() {
+        next_token = tokens[i];
+        i++;
+    }
+
+    write() {
+        fetch();
+        switch(next_token.first) {
+
+            case "WRITE":
+                return -1;
+            case "WORD":
+                return word();
+            case "NUMBER":
+                return number();
+            case "ENDPHRASE":
+                return -1;
+
+        }
+
+    }
+
+    number() {
+        fetch();
+        switch(next_token.first) {
+
+            case "WRITE":
+                return -1;
+            case "WORD":
+                return word();
+            case "NUMBER"
+                return number();
+            case "ENDPHRASE"
+                return 0;
+
+        }
+    }
+
+    word() {
+
+        fetch();
+        switch(next_token.first) {
+
+            case "WRITE":
+                return -1;
+            case "WORD":
+                return word();
+            case "NUMBER":
+                return number();
+            case "ENDPHRASE":
+                return 0;
+
+        }
+
+    }
 
 };
 
